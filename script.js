00b0f3a16e26d3a4761b6e42d4d3b58041dc54bf
Maxym Rusnachenko <88180470+r0zymnuk@users.noreply.github.com>
2021-07-30 09:49:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//NAVIGATION
+let active = 'income'
+
+let income = document.getElementById('income')
+let spendings = document.querySelector('#spendings')
+let moneybox = document.querySelector('#moneybox')
+let toDoList = document.querySelector('#to-do-list')
+
+let mainInput = document.querySelector('#main-input')
+let mainFrame = document.querySelector('#main-frame')
+
+function changingContent(clicked_id){
+    
+    document.querySelector('#' + clicked_id).classList.toggle('active')
+    document.querySelector('#' + active).classList.toggle('active')
+    active = clicked_id
+
+    mainFrame.src = 'pages/' + clicked_id + '/' + clicked_id + '.html'
+    if(clicked_id == 'moneybox'){
+        mainFrame.style.height = '100vh'
+    }else if(clicked_id == 'income'){
+        mainFrame.style.height = '60vh'
+    }else {
+        mainFrame.style.height = '40vh'
+    }
+    
+}
